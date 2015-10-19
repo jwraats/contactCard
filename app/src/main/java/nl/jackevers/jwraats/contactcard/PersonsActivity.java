405c75d557e33c0f428a0a2a415c0ed53a509f89
Jan Willem Raats <jwraats@JW-MacBook.local>
2015-10-19 20:34:19 +0200

    Added landscape mode

--- app/src/main/java/nl/jackevers/jwraats/contactcard/PersonsActivity.java
+++ app/src/main/java/nl/jackevers/jwraats/contactcard/PersonsActivity.java
@@ -1,27 +1,44 @@
 package nl.jackevers.jwraats.contactcard;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PersonsActivity extends AppCompatActivity implements PersonListFragment.OnFragmentInteractionListener{
+public class PersonsActivity extends AppCompatActivity implements PersonListFragment.OnFragmentInteractionListener, PersonFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persons);
+
     }
 
     @Override
     public void onFragmentInteraction(String msg) {
-        // Doorsturen naar andere Fragments ...
-//        PersonListFragment info = (PersonListFragment)
-//                getFragmentManager().findFragmentById(R.id.person_list_fragment);
-//
-//        // In Landscape, info != null
-//        if (info != null ) {
-//
-//        }else{
-//
-//        }
+        //Doorsturen naar andere Fragments ...
+        PersonListFragment info = (PersonListFragment)
+                getFragmentManager().findFragmentById(R.id.person_list_fragment);
+
+        // In Landscape, info != null
+        if (info != null ) {
+            //
+        }else{
+            //
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //Person Fragment
+        //Doorsturen naar andere Fragments ...
+        PersonListFragment info = (PersonListFragment)
+                getFragmentManager().findFragmentById(R.id.person_list_fragment);
+
+        // In Landscape, info != null
+        if (info != null ) {
+            //
+        }else{
+            //
+        }
     }
 }
